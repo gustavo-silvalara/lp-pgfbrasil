7aae7d6f6af7da0185188ca884f193270a382cb9
gustavo-silvalara <gustavo.silvalara@outlook.com>
2020-07-16 23:29:26 -0400

    pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { RowComponent } from './components/row/row.component';
+import { QuemSomosComponent } from './pages/quem-somos/quem-somos.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CabecalhoComponent,
+    TabsComponent,
+    RowComponent,
+    QuemSomosComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatProgressBarModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/quem-somos/quem-somos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quem-somos',
+  templateUrl: './quem-somos.component.html',
+  styleUrls: ['./quem-somos.component.scss'],
+})
+export class QuemSomosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
